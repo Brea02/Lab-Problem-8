47603394e4784149de2775432b67def28e20b7db
Breanna <b-sproul@onu.edu>
2023-10-19 13:11:13 -0700

    Initialized Visual Studio Project (test)

--- Lab Problem 8/Lab Problem 8.cpp
+++ Lab Problem 8/Lab Problem 8.cpp
@@ -1,1 +1,24 @@
 //Lab Problem 8
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+//function prototypes
+int countCharacter(string str);
+
+int main() {
+	cout << "Enter a word to be checked: ";
+	string str;
+	cin >> str;
+
+	cout << "The word has " << countCharacter(str) << " characters." << endl;
+
+}
+
+//countCharacter: counts the number of characters in a string
+//@param: string value to be checked
+//@return: int value of the number of characters in a string
+int countCharacter(string str) {
+
+}
